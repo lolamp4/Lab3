87bc8fab96ad768216894e4e1b1851793c43bc0c
Amazing Lawal <amazing.lawal@mail.utoronto.ca>
2024-09-29 19:26:38 -0400

    completed TODO

--- src/main/java/org/translation/JSONTranslationExample.java
+++ src/main/java/org/translation/JSONTranslationExample.java
@@ -42,9 +42,6 @@ public class JSONTranslationExample {
         return canada.getString("es");
     }
 
-    // TODO Task: Complete the method below to generalize the above to get the country name
-    //            for any country code and language code from sample.json.
-
     /**
      * Returns the name of the country based on the provided country and language codes.
      * @param countryCode the country, as its three-letter code.
@@ -52,6 +49,12 @@ public class JSONTranslationExample {
      * @return the translation of country to the given language or "Country not found" if there is no translation.
      */
     public String getCountryNameTranslation(String countryCode, String languageCode) {
+        for (int i = 0; i < jsonArray.length(); i++) {
+            JSONObject jsonObject = jsonArray.getJSONObject(i);
+            if (jsonObject.getString("alpha3").equals(countryCode)) {
+                return jsonObject.getString(languageCode);
+            }
+        }
         return "Country not found";
     }
 
