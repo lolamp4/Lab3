db25637d6f5a133b114029311d03053c2f705146
Amazing Lawal <amazing.lawal@mail.utoronto.ca>
2024-09-28 23:54:54 -0400

    populated languageMap dictionary, methods completed

--- src/main/java/org/translation/LanguageCodeConverter.java
+++ src/main/java/org/translation/LanguageCodeConverter.java
@@ -5,7 +5,6 @@ import java.net.URISyntaxException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.HashMap;
-import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 
@@ -14,12 +13,12 @@ import java.util.Map;
  */
 public class LanguageCodeConverter {
 
-    // TODO Task: pick appropriate instance variables to store the data necessary for this class
-    private Map<String, String> langaugeMap = new HashMap<>();
+    private final Map<String, String> languageMap = new HashMap<>();
     /**
      * Default constructor which will load the language codes from "language-codes.txt"
-     * in the resources folder.
+     * in the "resources" folder.
      */
+
     public LanguageCodeConverter() {
         this("language-codes.txt");
     }
@@ -34,19 +33,12 @@ public class LanguageCodeConverter {
         try {
             List<String> lines = Files.readAllLines(Paths.get(getClass()
                     .getClassLoader().getResource(filename).toURI()));
-
-            // TODO Task: use lines to populate the instance variable
-            //           tip: you might find it convenient to create an iterator using lines.iterator()
             lines.remove(0);
             for (String line : lines) {
-                String[] parts = line.split("\t");
-                if (!parts[0].contains(",")) {
-                    System.out.println(parts[0]);
-
-                }
-                String[] multi = parts[0].split(", ");
+                String[] values = line.split("\\t+");
+                this.languageMap.put(values[values.length - 1], values[0]);
             }
-            System.out.println(langaugeMap);
+
         }
         catch (IOException | URISyntaxException ex) {
             throw new RuntimeException(ex);
@@ -60,8 +52,7 @@ public class LanguageCodeConverter {
      * @return the name of the language corresponding to the code
      */
     public String fromLanguageCode(String code) {
-        // TODO Task: update this code to use your instance variable to return the correct value
-        return code;
+        return this.languageMap.get(code);
     }
 
     /**
@@ -70,8 +61,12 @@ public class LanguageCodeConverter {
      * @return the 2-letter code of the language
      */
     public String fromLanguage(String language) {
-        // TODO Task: update this code to use your instance variable to return the correct value
-        return language;
+        for (String key : this.languageMap.keySet()) {
+            if (this.languageMap.get(key).equals(language)) {
+                return key;
+            }
+        }
+        return null;
     }
 
     /**
@@ -79,7 +74,6 @@ public class LanguageCodeConverter {
      * @return how many languages are included in this code converter.
      */
     public int getNumLanguages() {
-        // TODO Task: update this code to use your instance variable to return the correct value
-        return this.langaugeMap.size();
+        return this.languageMap.size();
     }
 }
